4627bbcf58d8a9b1ae3fe98c1416fda6d30e9d77
Susan Enneking <susan.enneking@gmail.com>
2014-09-24 05:58:46 -0500

    Adoptee schema

--- public/app/adoptees/mvAdopteeListCtrl.js
+++ public/app/adoptees/mvAdopteeListCtrl.js
@@ -2,6 +2,6 @@ angular.module('app').controller('mvAdopteeListCtrl', function($scope, mvCachedA
   $scope.adoptees = mvCachedAdoptees.query();
 
   $scope.sortOptions = [{value:"name",text: "Sort by Name"},
-    {value: "enrolled",text: "Sort by Enroll Date"}];
+    {value: "created",text: "Sort by Create Date"}];
   $scope.sortOrder = $scope.sortOptions[0].value;
 });
--- server/models/Adoptee.js
+++ server/models/Adoptee.js
@@ -1,19 +1,82 @@
 var mongoose = require('mongoose');
 
+var phoneTypes = ['Home',
+    'Cell1',
+    'Cell2',
+    'Other']
+
+var phone = {
+    type: {type: String, enum: phoneTypes},
+    name: {type: String},
+    number: {type: String}
+}
+
+var householdTypes = ['Single',
+    'Adult Only',
+    'Single Mom with Children',
+    'Single Dad with Children',
+    'Married Couple with Children',
+    'Adult with Children',
+    'Grandparents (only) with Children']
+var states = ['KS', 'MO', 'NE', 'OK', 'CO'] //most of the current records have null states...most addresses are topeka
+
+var adopteeStates = ['In Process',
+    'Not Matched',
+    'Matched']
+var genders = ['Male', 'Female']
+
 var adopteeSchema = mongoose.Schema({
-  name: {type:String, required:'{PATH} is required!'},
-  isSingleParent: {type:Boolean, required:'{PATH} is required!'},
-  enrolled: {type:Date, required:'{PATH} is required!'},
-  tags: [String]
-});
+    firstName: {type:String, required:'{PATH} is required!'},
+    lastName: {type:String, required:'{PATH} is required!'},
+    middleInitial: {type: String},
+    birthDate: {type: Date},
+    ssnLastFour: {type: Number, max: 9999},
+    gender: {type: String, enum: genders},
+    //_agentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent' },
+    _adopterId: { type: mongoose.Schema.Types.ObjectId, ref: 'Adopter' },
+    householdType: {type:String, enum: householdTypes},
+    address: {
+        homeAddress: {type: String},
+        city: {type: String},
+        state: {type: String, enum: states},
+        zip: {type: String}
+    },
+    phones: [
+        phone
+    ],
+    email: {type: String},
+    fax: {type: String},
+    status: {type: String, enum: adopteeStates},
+    applicationNumber: {type: String},
+    isSpanishOnly: {type: Boolean},
+    englishSpeakingPerson: {type: String},
+    isDisabled: {type: Boolean},
+    isSenior: {type: Boolean},
+    isVeteran: {type: Boolean},
+    isHomebound: {type: Boolean},
+    isDiabetic: {type: Boolean},
+    isAllergic: {type: Boolean},
+    reactionFoods: {type: String},
+    story: {type: String},
+    volunteerComment: {type: String},
+    internalComment: {type: String},
+    createDate: {type: Date},
+    _createUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    modifyDate: {type: Date},
+    _modifyUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    });
 var Adoptee = mongoose.model('Adoptee', adopteeSchema);
 
 function createDefaultAdoptees() {
   Adoptee.find({}).exec(function(err, collection) {
     if(collection.length === 0) {
-      Adoptee.create({name: 'Jane Seymour', isSingleParent: true, enrolled: new Date('10/5/2013'), tags: ['Nice lady']});
-      Adoptee.create({name: 'John Wayne', isSingleParent: false, enrolled: new Date('10/12/2013'), tags: ['Five grandchildren']});
-      Adoptee.create({name: 'Marge Simpson', isSingleParent: false, enrolled: new Date('10/1/2013'), tags: ['Being married to Homer is like being single']});
+      Adoptee.create({firstName: 'Jane', lastName: 'Seymour', gender: 'Female',
+          birthDate: new Date('01/21/1958'), createDate: new Date('01/21/2014'), ssnLastFour: 9998,
+          phones: [{phone: {type: 'Home', name: 'Jane', number: '785 865 8111'}},
+                   {phone: {type: 'Cell1', name: 'Jane', number: '785 550 2111'}}]
+      });
+      Adoptee.create({firstName: 'Mark', lastName: 'Lark', gender: 'Male', birthDate: new Date('07/09/1985'), createDate: new Date('09/01/2014'), ssnLastFour: 9997});
+      Adoptee.create({firstName: 'James', lastName: 'Brown', gender: 'Male', birthDate: new Date('11/26/1978'), createDate: new Date('06/21/2014'), ssnLastFour: 0909});
     }
   })
 }
