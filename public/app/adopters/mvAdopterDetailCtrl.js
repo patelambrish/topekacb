791cc0be65919e83187523afee110d6be4c25fee
thombrg <thombr@gmail.com>
2014-09-27 12:28:08 -0500

    refactored adopter detail
    removed use of cachedadopter
    added ability to handle add-new

--- public/app/adopters/mvAdopterDetailCtrl.js
+++ public/app/adopters/mvAdopterDetailCtrl.js
@@ -1,12 +1,12 @@
 /**
  * Created by Susan on 8/19/2014.
  */
-angular.module('app').controller('mvAdopterDetailCtrl', function($scope, mvCachedAdopters, $routeParams) {
-    mvCachedAdopters.query().$promise.then(function(collection) {
-        collection.forEach(function(adopter) {
-            if(adopter._id === $routeParams.id) {
-                $scope.adopter = adopter;
-            }
-        })
-    })
-});
+angular.module('app').
+	controller('mvAdopterDetailCtrl', 
+	function($scope, $routeParams, mvAdopter) {
+        if($routeParams.id) {
+            $scope.adopter = mvAdopter.get({ _id: $routeParams.id });
+        } else {
+            $scope.adopter = new mvAdopter();
+        }
+	});
