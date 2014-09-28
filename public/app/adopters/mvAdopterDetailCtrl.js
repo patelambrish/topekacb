4290a89a3d7cb86471fcacc819b96e1c106395ef
thombrg <thombr@gmail.com>
2014-09-27 21:35:39 -0500

    converted to 2-space indent from 4-space
    switched to using caret ide

--- public/app/adopters/mvAdopterDetailCtrl.js
+++ public/app/adopters/mvAdopterDetailCtrl.js
@@ -2,11 +2,10 @@
  * Created by Susan on 8/19/2014.
  */
 angular.module('app').
-	controller('mvAdopterDetailCtrl', 
-	function($scope, $routeParams, mvAdopter) {
-        if($routeParams.id) {
-            $scope.adopter = mvAdopter.get({ _id: $routeParams.id });
-        } else {
-            $scope.adopter = new mvAdopter();
-        }
+	controller('mvAdopterDetailCtrl', function($scope, $routeParams, mvAdopter) {
+    if($routeParams.id) {
+      $scope.adopter = mvAdopter.get({ _id: $routeParams.id });
+    } else {
+      $scope.adopter = new mvAdopter();
+    }
 	});
--- public/app/adopters/mvAdopterListCtrl.js
+++ public/app/adopters/mvAdopterListCtrl.js
@@ -2,51 +2,51 @@
  * Created by Susan on 8/19/2014.
  */
 angular.module('app').
-    filter("startFrom", function() {
-        return function(array, start) {
-            start = parseInt(start, 10);
-            
-            return (angular.isArray(array) || angular.isString(array)) && start ? array.slice(start) : array;
-        };
-    }).
-    controller('mvAdopterListCtrl', function($scope, $filter, mvAdopter) {
-        var adopters = mvAdopter.query();
+  filter("startFrom", function() {
+    return function(array, start) {
+      start = parseInt(start, 10);
+      
+      return (angular.isArray(array) || angular.isString(array)) && start ? array.slice(start) : array;
+    };
+  }).
+  controller('mvAdopterListCtrl', function($scope, $filter, mvAdopter) {
+    var adopters = mvAdopter.query();
 
-        $scope.sort = {
-            value: '-enrolled',
-            text: 'Enroll Date: Recent to Old',
-            options: [
-                {value: 'name', text: 'Name'},
-                {value: 'enrolled', text: 'Enroll Date: Old to Recent'},
-                {value: '-enrolled', text: 'Enroll Date: Recent to Old'}
-            ]
-        };
-        
-        $scope.page = {
-            current: 1,
-            total: 1,
-            previous: 1,
-            next: 1,
-            size: 10
-        };
-        
-        $scope.applySort = function(sortOption) {
-            angular.extend($scope.sort, sortOption);
-        };
-        
-        $scope.applyFilter = function(query) {
-            adopters.$promise.then(function() {
-                $scope.adopters = $filter('filter')(adopters, { name: query });
-                $scope.applyPage(1);
-            });
-        };
-        
-        $scope.applyPage = function(page) {
-            $scope.page.current = page;
-            $scope.page.total = Math.ceil($scope.adopters.length / $scope.page.size);
-            $scope.page.previous = page > 1 ? page - 1 : page;
-            $scope.page.next = page < $scope.page.total ? page + 1 : page;
-        };
-        
-        $scope.applyFilter();
-    });
+    $scope.sort = {
+      value: '-enrolled',
+      text: 'Enroll Date: Recent to Old',
+      options: [
+        {value: 'name', text: 'Name'},
+        {value: 'enrolled', text: 'Enroll Date: Old to Recent'},
+        {value: '-enrolled', text: 'Enroll Date: Recent to Old'}
+      ]
+    };
+    
+    $scope.page = {
+      current: 1,
+      total: 1,
+      previous: 1,
+      next: 1,
+      size: 10
+    };
+    
+    $scope.applySort = function(sortOption) {
+      angular.extend($scope.sort, sortOption);
+    };
+    
+    $scope.applyFilter = function(query) {
+      adopters.$promise.then(function() {
+        $scope.adopters = $filter('filter')(adopters, query);
+        $scope.applyPage(1);
+      });
+    };
+    
+    $scope.applyPage = function(page) {
+      $scope.page.current = page;
+      $scope.page.total = Math.ceil($scope.adopters.length / $scope.page.size);
+      $scope.page.previous = page > 1 ? page - 1 : page;
+      $scope.page.next = page < $scope.page.total ? page + 1 : page;
+    };
+    
+    $scope.applyFilter();
+  });
