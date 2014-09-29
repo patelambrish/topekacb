630b9c7ed8f0144627c9cbd81e7ce9d8cd98cfc8
patelambrish <ambrish.patel@se2.com>
2014-09-28 22:12:21 -0500

    User Management
    User management with facebook registration & login

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,51 +2,52 @@ var auth = require('./auth'), users = require('../controllers/users'), adoptees 
 
 module.exports = function(app) {
 
-  app.get('/api/users', auth.requiresRole('admin'), users.getUsers);
-  app.post('/api/users', users.createUser);
-  app.put('/api/users', users.updateUser);
+	app.get('/api/users', auth.requiresRole('admin'), users.getUsers);
+	app.post('/api/users', users.createUser);
+	app.put('/api/users', users.updateUser);
 
-  app.get('/api/adoptees', auth.requiresRole('user'), adoptees.getAdoptees);
-  app.get('/api/adoptees/:id', adoptees.getAdopteeById);
+	app.get('/api/adoptees', auth.requiresRole('user'), adoptees.getAdoptees);
+	app.get('/api/adoptees/:id', adoptees.getAdopteeById);
 
-  app.get('/api/adopters', auth.requiresRole('user'), adopters.getAdopters);
-  app.get('/api/adopters/:id', adopters.getAdopterById);
+	app.get('/api/adopters', auth.requiresRole('user'), adopters.getAdopters);
+	app.get('/api/adopters/:id', adopters.getAdopterById);
 
-  app.get('/partials/*', function(req, res) {
-    res.render('../../public/app/' + req.params[0]);
-  });
+	app.get('/partials/*', function(req, res) {
+		res.render('../../public/app/' + req.params[0]);
+	});
 
-  app.post('/login', auth.authenticate);
+	app.post('/login', auth.authenticate);
 
-  app.post('/logout', function(req, res) {
-    req.logout();
-    res.end();
-  });
+	app.post('/logout', function(req, res) {
+		req.logout();
+		res.end();
+	});
 
-  app.all('/api/*', function(req, res) {
-    res.send(404);
-  });
+	app.all('/api/*', function(req, res) {
+		res.send(404);
+	});
 
-  app.get('/auth/facebook', passport.authenticate('facebook'));
+	app.get('/auth/facebook', passport.authenticate('facebook'));
 
-  app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-    failureRedirect: '/', successRedirect: '/'
-  }));
+	app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+		failureRedirect : '/facebook/You are registered. Please contact system administrator!',
+		successRedirect : '/facebook/You have successfully signed in!'
+	}));
 
-  app.get('*', function(req, res) {
-    var rUser;
-    if(req.user) {
-      rUser = {
-        username: req.user.username,
-        firstName: req.user.firstName,
-        lastName: req.user.lastName,
-        id: req.user._id,
-        roles: req.user.roles
-      };
-    }
-    res.render('index', {
-      bootstrappedUser : rUser
-    });
-  });
+	app.get('*', function(req, res) {
+		var rUser;
+		if (req.user) {
+			rUser = {
+				username : req.user.username,
+				firstName : req.user.firstName,
+				lastName : req.user.lastName,
+				id : req.user._id,
+				roles : req.user.roles
+			};
+		}
+		res.render('index', {
+			bootstrappedUser : rUser
+		});
+	});
 
 }
