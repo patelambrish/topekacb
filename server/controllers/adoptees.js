784c343f1d86a3664eb6c560e00e7b9aa6af7fd6
Susan Enneking <susan.enneking@gmail.com>
2014-10-01 05:58:48 -0500

    Household member ui support

--- server/controllers/adoptees.js
+++ server/controllers/adoptees.js
@@ -12,43 +12,27 @@ exports.getAdopteeById = function(req, res) {
     })
 };
 exports.updateAdoptee = function(req, res){
-    var adopteeUpdates = req.body;
     var userId = req.user ? req.user._id : null;
-    Adoptee.findOne({_id: adopteeUpdates._id}).exec(function(err, curAdoptee) {
-        curAdoptee.firstName = adopteeUpdates.firstName;
-        curAdoptee.lastName = adopteeUpdates.lastName;
-        curAdoptee.middleInitial = adopteeUpdates.middleInitial;
-        curAdoptee.birthDate = adopteeUpdates.birthDate;
-        curAdoptee.ssnLastFour = adopteeUpdates.ssnLastFour;
-        curAdoptee.gender = adopteeUpdates.gender;
-        curAdoptee.address = adopteeUpdates.address;
-        curAdoptee.homePhone = adopteeUpdates.homePhone;
-        curAdoptee.cell1Phone = adopteeUpdates.cell1Phone;
-        curAdoptee.cell2Phone = adopteeUpdates.cell2Phone;
-        curAdoptee.email = adopteeUpdates.fax;
-        curAdoptee.status = adopteeUpdates.status;
-        curAdoptee.applicationNumber = adopteeUpdates.applicationNumber;
-        curAdoptee.language = adopteeUpdates.language;
-        curAdoptee.englishSpeaker = adopteeUpdates.englishSpeaker;
-        curAdoptee.isDisabled = adopteeUpdates.isDisabled;
-        curAdoptee.isHomebound = adopteeUpdates.isHomebound;
-        curAdoptee.isSenior = adopteeUpdates.isSenior;
-        curAdoptee.isVeteran = adopteeUpdates.isVeteran;
-        curAdoptee.isDiabetic = adopteeUpdates.isAllergic;
-        curAdoptee.reactionFoods = adopteeUpdates.reactionFoods;
-        curAdoptee.story = adopteeUpdates.story;
-        curAdoptee.volunteerComment = adopteeUpdates.volunteerComment;
-        curAdoptee.internalComment = adopteeUpdates.internalComment;
-        if(! curAdoptee._id) {
-            curAdoptee.createDate = new Date();
-            curAdoptee.createdBy = userId;
-        } else {
-            curAdoptee.modifiedDate = new Date();
-            curAdoptee.modifiedBy = userId;
-        }
-        curAdoptee.save(function(err){
+    var update = req.body,
+        id = update._id,
+        options = { upsert: true },
+        userId = req.user ? req.user._id : null;
+    if(!id) {
+        id = new mongoose.Types.ObjectId();
+        update.createDate = new Date();
+        update.createdBy = userId;
+    } else {
+        update.modifiedDate = new Date();
+        update.modifiedBy = userId;
+    }
+    delete update.__v; //todo:  tried .select('-__v') with error on put  more research required
+    Adoptee.
+        findByIdAndUpdate(id, update, options).
+        populate('createdBy', 'firstName lastName').
+        populate('modifiedBy', 'firstName lastName').
+        exec(function(err, adoptee) {
             if(err) { res.status(400); return res.send({message:err.toString()});}
             return res.send({message: "Adoptee successfully updated!"})
-            });
-        })
+        });
+
     }
