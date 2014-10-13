6b9cba97072f5cdb7ceae1d3175f0619cdba6642
patelambrish <ambrish.patel@se2.com>
2014-10-13 11:25:02 -0500

    Made style changes to match with other views. Also, added error handling

--- public/app/common/mvNotifier.js
+++ public/app/common/mvNotifier.js
@@ -4,6 +4,9 @@ angular.module('app').factory('mvNotifier', function(mvToastr) {
   return {
     notify: function(msg) {
       mvToastr.success(msg);
+    },
+    notifyError: function(msg) {
+     mvToastr.error(msg);
     }
   }
 })
