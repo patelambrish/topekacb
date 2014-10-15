5b84adf3ed989ce2a5b840c543d3abaeba5204d2
patelambrish <ambrish.patel@se2.com>
2014-10-14 19:56:36 -0500

    connect-mongodb for session storage in mongo
    run 'npm install' to get node package

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ var app = express();
 
 var config = require('./server/config/config')[env];
 
-require('./server/config/express')(app, config);
-
 require('./server/config/mongoose')(config);
+
+require('./server/config/express')(app, config);
 
 require('./server/config/passport')(config);
 
--- server/config/express.js
+++ server/config/express.js
@@ -1,11 +1,13 @@
 var newrelic = require('newrelic');
 
 var express = require('express'),
-    logger = require('morgan'),
-    bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
-    session = require('express-session'),
-    passport = require('passport');
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  passport = require('passport'),
+  mongoose = require('mongoose'),
+  MongoStore = require('connect-mongodb');
 
 module.exports = function(app, config) {
   app.set('views', config.rootPath + '/server/views');
@@ -13,13 +15,17 @@ module.exports = function(app, config) {
   app.use(logger('dev'));
   app.use(cookieParser());
   app.use(bodyParser());
-  app.use(session({secret: 'Topeka unicorns',
-                 saveUninitialized: true,
-                 resave: true}));
+  app.use(session({
+	secret : 'Topeka unicorns',
+	store : new MongoStore({url: config.db, reapInterval: 60 * 60 * 1000}),
+	saveUninitialized: true,
+    resave: true,
+    cookie: {
+      maxAge: 720 * 60 * 1000
+    }
+  }));
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(express.static(config.rootPath + '/public'));
-
   app.locals.newrelic = newrelic;
-
-}
+};
