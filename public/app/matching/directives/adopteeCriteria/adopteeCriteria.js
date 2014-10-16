bf201387648f02c54ba3acfd6c2dd62538845cb2
Susan Enneking <susan.enneking@gmail.com>
2014-10-16 05:26:40 -0500

    Adoptee match work in progress

--- public/app/matching/directives/adopteeCriteria/adopteeCriteria.js
+++ public/app/matching/directives/adopteeCriteria/adopteeCriteria.js
@@ -4,10 +4,10 @@ angular.module('app').directive('adopteeCriteria', [
             templateUrl : '/partials/matching/directives/adopteeCriteria/adopteeCriteria',
             restrict: 'A',
             controller: ['$scope', function($scope) {
-                $scope.adopteeEnums;;
+                $scope.adopteeEnums;
+
                 $scope.select = function(adoptee){
                     $scope.adopteeEnums = adoptee.enums;
-                    $scope.$emit('adopteeSelected', adoptee.criteria);
                 }
             }]
         };
