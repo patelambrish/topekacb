4d9bbc0bbe9528e211e20f1e72bbc392f6af3e9c
patelambrish <ambrish.patel@se2.com>
2014-10-15 20:08:00 -0500

    Changing from connect-mongodb to connect-mongo to use existing mongoose connection

--- server/config/express.js
+++ server/config/express.js
@@ -1,32 +1,36 @@
 var newrelic = require('newrelic');
 
 var express = require('express'),
-  logger = require('morgan'),
-  bodyParser = require('body-parser'),
-  cookieParser = require('cookie-parser'),
-  session = require('express-session'),
-  passport = require('passport'),
-  mongoose = require('mongoose'),
-  MongoStore = require('connect-mongodb');
+	logger = require('morgan'),
+	bodyParser = require('body-parser'),
+	cookieParser = require('cookie-parser'),
+	session = require('express-session'),
+	passport = require('passport'),
+	mongoose = require('mongoose'),
+	MongoStore = require('connect-mongo')(session);
 
 module.exports = function(app, config) {
-  app.set('views', config.rootPath + '/server/views');
-  app.set('view engine', 'jade');
-  app.use(logger('dev'));
-  app.use(cookieParser());
-  app.use(bodyParser());
-  app.use(session({
-	secret : 'Topeka unicorns',
-	store : new MongoStore({url: config.db, reapInterval: 10 * 60 * 1000}),
-	saveUninitialized: true,
-    resave: true,
-    cookie: {
-      maxAge: 60 * 60 * 1000
-    },
-    rolling: true
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(express.static(config.rootPath + '/public'));
-  app.locals.newrelic = newrelic;
+	console.log('express');
+	app.set('views', config.rootPath + '/server/views');
+	app.set('view engine', 'jade');
+	app.use(logger('dev'));
+	app.use(cookieParser());
+	app.use(bodyParser());
+	app.use(session({
+		secret : 'Topeka unicorns',
+		store : new MongoStore({
+			mongoose_connection : mongoose.connections[0]
+		}),
+		saveUninitialized : true,
+		resave : true,
+		cookie : {
+			maxAge : 60 * 60 * 1000
+		},
+		rolling : true
+	}));
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(express.static(config.rootPath + '/public'));
+	app.locals.newrelic = newrelic;
+
 };
