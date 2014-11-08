4eae6c272fe77c05bac7cfef3c7c7101bfa5f62b
thombrg <thombr@gmail.com>
2014-11-08 11:55:49 -0600

    refactor sample data generation (repeat)
    (repeat from pull request #99)
    
    refactor portions of data generation pertaining matching and status to
    more accurately reflect real world and to better facilitate matching
    testing

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -4,25 +4,45 @@ var mongoose = require('mongoose'),
     adopterModel = require('../models/Adopter'),
     stateModel = require('../models/State'),
     messageModel = require('../models/Message'),
-    adopteeApplicationCounterModel = require('../models/AdopteeApplicationCounter'),
-    printEmailModel = require('../models/PrintEmail.js');
+    adopteeApplicationCounterModel = require('../models/AdopteeApplicationCounter');
 
 module.exports = function(config) {
   mongoose.connect(config.db);
-  var db = mongoose.connection;
+  var db = mongoose.connection,
+      sampleAdopters, sampleAdoptees;
+    
   db.on('error', console.error.bind(console, 'connection error...'));
   db.once('open', function callback() {
     console.log('topekaCb db opened');
   });
+
   console.log("Environment:  " + process.env.NODE_ENV);
-  if (process.env.NODE_ENV == 'development' || process.env.NODE_ENV == "test"){
+
+  if(process.env.NODE_ENV == 'development' || process.env.NODE_ENV == "test") {
     userModel.createDefaultUsers();
     stateModel.createStates();
-    adopteeModel.createDefaultAdoptees();
-    adopterModel.createDefaultAdopters();
+
+    adopterModel.createSampleAdopters().
+      then(function(data) {
+        sampleAdopters = data;
+        return adopteeModel.createSampleAdoptees();
+      }).
+      then(function(data) {
+        sampleAdoptees = data.filter(function(a) {
+          return a.status === 'Not Matched';
+        });
+        sampleAdoptees.length = Math.floor(sampleAdoptees.length * 0.88);
+        console.log('Creating sample matches from a pool of ' + sampleAdoptees.length + ' adoptees...');
+        sampleAdopters.forEach(function(adopter) {
+          adopterModel.createSampleMatch(adopter, sampleAdoptees);
+        });
+        process.stdout.write('\n');
+        console.log('Successfully completed sample matching.');
+      });
+
     adopteeApplicationCounterModel.initializeAdopteeApplicationCounter();
     messageModel.createMessages();
   }
+
   adopteeModel.startOrphanedUpdateChecking();
 };
-
