061c21337aa60fa01db11487b521ca33c02449ec
patelambrish <Ambrish.Patel@se2.com>
2017-11-09 12:31:41 -0600

    Phone number updated - Email content

--- server/controllers/printEmailRequest.js
+++ server/controllers/printEmailRequest.js
@@ -166,7 +166,7 @@ exports.email = function(req, res, next) {
 						to : emailTo,
 						from : config.emailFrom,
 						subject : config.emailSubject,
-						html : 'Dear Adopter, <br/><br/> Thank you so much for adopting a family this year through the community Christmas Bureau. With your generosity we will be able to make sure our community friends have Christmas for their family. <br/><br/> To help you with what to do next please attached find an informational letter along with  a list of your family or families you have adopted for this holiday season. <br/><br/> If you have any questions or comments please do not hesitate to contact the Christmas Bureau staff at (785) 581-0234 or cb@unitedwaytopeka.org. <br/><br/> Thank you again and Merry Christmas to you and your family. <br/><br/>Kimberly Wolff<br/>Christmas Bureau<br/>United Way of Greater Topeka.',
+						html : 'Dear Adopter, <br/><br/> Thank you so much for adopting a family this year through the community Christmas Bureau. With your generosity we will be able to make sure our community friends have Christmas for their family. <br/><br/> To help you with what to do next please attached find an informational letter along with  a list of your family or families you have adopted for this holiday season. <br/><br/> If you have any questions or comments please do not hesitate to contact the Christmas Bureau staff at (785) 228-5120 or cb@unitedwaytopeka.org. <br/><br/> Thank you again and Merry Christmas to you and your family. <br/><br/>Kimberly Wolff<br/>Christmas Bureau<br/>United Way of Greater Topeka.',
 						files : [{
 									filename: '2017 Family Cost Table.pdf',
 									path: 'server/content/2017 Family Cost Table.pdf'
