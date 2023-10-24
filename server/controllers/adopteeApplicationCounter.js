c1478eb48a36af13bbe854214dfe0128be5b7b0a
ap <ambrishkpatel@gmail.com>
2023-10-23 19:40:08 -0500

    Update as part of mongoose upgrade

--- server/controllers/adopteeApplicationCounter.js
+++ server/controllers/adopteeApplicationCounter.js
@@ -5,8 +5,10 @@ exports.getNextSequence = function(req, res) {
   AdopteeApplicationCounter.
       findByIdAndUpdate('adopteeApplication', { $inc: { seq: 1 } }, { upsert: true, new: true }).
       then((returnVal) => {
-          if(err) { res.status(400); return res.send({error:err.toString()});}
+          
           return res.send(returnVal);
-  });
+      }).catch(err => {
+        if(err) { res.status(400); return res.send({error:err.toString()});}
+      });
 }
 
